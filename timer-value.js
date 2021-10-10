e6b857d503444effd9a6096023d6d155a1fcc134
xingmove <87740640+xingmove@users.noreply.github.com>
2021-10-10 18:51:48 +0900

    Add files via upload

---
+++ timer-value.js
@@ -0,0 +1,3 @@
+
+        var def = {"via":"web","name":"Custom Timer","type":0,"numberOfSets":1,"soundScheme":8,"intervals":[{"name":"Interval 1","duration":5,"color":"3","$$hashKey":"object:3"},{"name":"Interval 2","duration":2,"color":"2","$$hashKey":"object:15"},{"name":"Interval 3","duration":1,"color":"5","$$hashKey":"object:33"}]};
+       
